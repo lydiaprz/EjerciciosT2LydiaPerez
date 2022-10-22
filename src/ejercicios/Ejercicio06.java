c5bd66a2c861d1fd39a0fc5204cd26aadf05ca9a
Lydia <lydia.perez@iesnervion.es>
2022-10-22 17:05:07 +0200

    qq

--- src/ejercicios/Ejercicio06.java
+++ src/ejercicios/Ejercicio06.java
@@ -1,8 +1,25 @@
 package ejercicios;
 
+import java.util.Scanner;
+
 public class Ejercicio06 {
     public static void main(String[] args) {
         //declaramos las variables
-
+        int num1, num2, sumaUser, suma;
+        //declaramos el scanner
+        Scanner sc=new Scanner(System.in);
+        //inicializamos los numeros con numeros aleatorios
+        num1=(int) ((Math.random()*99)+1);
+        num2=(int) ((Math.random()*99)+1);
+        //le pedimos al usuario que calcule e introduzca la suma
+        System.out.println("Introduzca la suma de "+num1+" y "+num2+".");
+        sumaUser= sc.nextInt();
+        //comprobamos el resultado
+        suma=num1+num2;
+        if (sumaUser==suma){
+            System.out.println("La suma es correcta.");
+        }else {
+            System.out.println("La suma es incorrecta, el resultado es "+suma+".");
+        }
     }
 }
